96cb90d901ee1019566e94a7c765b66b1decec53
andrea <tamara11corra@gmail.com>
2022-09-29 15:28:53 -0500

    corrección ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import { Home } from './Home/Home';
+import { Home } from './home/Home';
 import { Playlist } from './Playlist/Playlist';
 import { Album } from './Album/Album';
 import { Artist } from './Artist/Artist';
